ec5ac9a7f660e136b655e5a30752bd4aea70086b
Afolabi Esther <starafolab@gmail.com>
2024-06-12 07:41:08 +0100

    fix 3

--- src/services/ProtectedRoute.jsx
+++ src/services/ProtectedRoute.jsx
@@ -3,9 +3,9 @@ import { Outlet, Navigate } from "react-router-dom";
 // import useAuth from "../features/authentication/AuthContext";
 
 function ProtectedRoute() {
-  // const isLoggedIn = true;
+  const isLoggedIn = true;
 
-    const isLoggedIn = localStorage.getItem("isLoggedIn");
+    // const isLoggedIn = localStorage.getItem("isLoggedIn");
   // console.log(token, isAuthenticated);
 
   return isLoggedIn ? <Outlet /> : <Navigate to={"/login"} />;
